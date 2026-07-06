6cae9ab85e5dbd04a921e4407df69a089c1df665
Lewis <38916472+Lewis-Hamilton@users.noreply.github.com>
2026-07-05 22:47:28 -0500

    switched reboot to restart

--- i3/.config/i3/scripts/power-menu.sh
+++ i3/.config/i3/scripts/power-menu.sh
@@ -8,5 +8,5 @@ chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu:" -theme-str 'window
 # Execute command based on choice
 case "$chosen" in
     *Shutdown) systemctl poweroff ;;
-    *Reboot) systemctl reboot ;;
+    *Restart) systemctl reboot ;;
 esac
