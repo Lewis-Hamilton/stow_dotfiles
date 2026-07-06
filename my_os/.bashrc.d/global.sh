cfe056a26a743ef73c06232e573bba636703cfab
Lewis <38916472+Lewis-Hamilton@users.noreply.github.com>
2026-07-06 00:41:36 -0500

    updated color

--- my_os/.bashrc.d/global.sh
+++ my_os/.bashrc.d/global.sh
@@ -1,4 +1,4 @@
-MY_GREEN="\[\e[0;32m\]"
+MY_ORANGE="\[\e[38;2;238;103;72m\]"
 DEFAULT="\[\e[0m\]"
 
-export PS1="${DEFAULT}[${MY_GREEN}\u@\h \W${DEFAULT}]\$ ${DEFAULT}"
+export PS1="${DEFAULT}[${MY_ORANGE}\u@\h \W${DEFAULT}]\$ ${DEFAULT}"
