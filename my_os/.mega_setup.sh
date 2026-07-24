dfca296d97deee91d55ee2c2fd2338f57773e92a
Lewis <38916472+Lewis-Hamilton@users.noreply.github.com>
2026-07-24 15:56:40 -0500

    added font install step

--- my_os/.mega_setup.sh
+++ my_os/.mega_setup.sh
@@ -7,6 +7,8 @@ MY_ORANGE='\033[38;2;238;103;72m'
 MY_YELLOW='\033[38;2;255;216;94m'
 MY_GREEN='\033[38;2;82;238;163m'
 DEFAULT='\033[0m'
+FONT_DIR="~/.local/share/fonts"
+FONT_URL="https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Ligatures/SemiBold/JetBrainsMonoNerdFont-SemiBold.ttf"
 
 success_output() {
     echo -e $MY_GREEN$1$DEFAULT
@@ -38,15 +40,25 @@ else
     exit 1
 fi
 
-echo "==> Checking DNF Packages"
-
-echo "==> Checking Flatpak Packages"
-
 echo "==> Checking Font"
+
 if fc-list : family style | grep -qi "JetBrainsMono.*SemiBold"; then
     success_output "Verified font is installed"    
 else
     warning_output "JetBrainsMono Nerd Font Mono-SemiBold is not installed"
+    echo "==> Installing Font"
+    
+    if mkdir -p "$FONT_DIR" && \
+        curl -fLo "$FONT_DIR/$FONT_NAME" "$RAW_URL" && \
+        fc-cache -f "$FONT_DIR"; then
+            success_output "Successfully installed font"
+        else
+            failed_output "Error: Failed to install Font"
+        fi
 fi
+
+echo "==> Checking DNF Packages"
+
+echo "==> Checking Flatpak Packages"
 
 echo "==> Setup complete!"
