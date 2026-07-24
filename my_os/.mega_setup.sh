318b042ab068a7dabccc2bc22dfe46615d0fd07c
Lewis <38916472+Lewis-Hamilton@users.noreply.github.com>
2026-07-23 20:40:45 -0500

    added color function

--- my_os/.mega_setup.sh
+++ my_os/.mega_setup.sh
@@ -8,6 +8,14 @@ MY_YELLOW='\033[38;2;255;216;94m'
 MY_GREEN='\033[38;2;82;238;163m'
 DEFAULT='\033[0m'
 
+success_output() {
+    echo -e $MY_GREEN$1$DEFAULT
+}
+
+failed_output() {
+    echo -e $MY_RED$1$DEFAULT
+}
+
 echo "==> Starting Setup"
 echo "==> Checking Distro"
 
@@ -15,14 +23,14 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     DISTRO=$ID
     if [[ "$DISTRO" = "fedora" ]]; then
-        echo -e "${MY_GREEN}Verified this machine is running Fedora${DEFAULT}"
+        success_output "Verified this machine is running Fedora"
     else
-        echo -e "${MY_RED}Error: This machine is not running Fedora${DEFAULT}"
+        failed_output "Error: This machine is not running Fedora"
         echo "This setup is specific to Fedora."
         exit 1
     fi
 else
-    echo -e "${MY_RED}Error: Failed to detect Linux distribution${DEFAULT}"
+    failed_output "Error: Failed to detect Linux distribution"
     exit 1
 fi
 
